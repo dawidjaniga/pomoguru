9af53858fba861db9fe12af5f7928a8fc1da2f80
Dawid Janiga <dawid.janiga@netguru.com>
2022-03-28 22:27:27 +0200

    feat(socketio): use api url

--- packages/client/src/SocketIoRealTimeProvider.ts
+++ packages/client/src/SocketIoRealTimeProvider.ts
@@ -1,11 +1,20 @@
 import { io, Socket } from 'socket.io-client'
 
+const apiUrl = process.env['NX_POMOGURU_API_URL']
+
+if (!apiUrl) {
+  throw new Error('NX_POMOGURU_API_URL is not defined')
+}
 export class SocketIoRealTimeProvider {
   private socket: Socket
   // private userChannel: Channel
 
   constructor () {
-    this.socket = io('http://localhost:4000')
+    if (!apiUrl) {
+      throw new Error('NX_POMOGURU_API_URL is not defined')
+    }
+
+    this.socket = io(apiUrl)
 
     this.attachEvents()
   }
