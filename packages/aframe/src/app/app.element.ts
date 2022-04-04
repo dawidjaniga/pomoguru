e36768574173c8376a3911f18bab4522c6e5b223
Dawid Janiga <dawid.janiga@netguru.com>
2022-04-04 12:06:35 +0200

    refactor(aframe): split responsibility by elements

--- packages/aframe/src/app/app.element.ts
+++ packages/aframe/src/app/app.element.ts
@@ -3,52 +3,80 @@ import 'aframe'
 import 'aframe-environment-component'
 import { controller, model, TimeLeft } from '@pomoguru/client'
 
-function init () {
-  const body = document.body
-  const sceneEl = document.createElement('a-scene')
-  sceneEl.setAttribute('cursor', 'rayOrigin: mouse')
+class AframeApp {
+  private sceneEl: HTMLElement
 
-  const startTimerEl = document.createElement('a-box')
-  startTimerEl.setAttribute('color', 'green')
-  startTimerEl.setAttribute('position', '-4 1 -10')
-  startTimerEl.setAttribute('scale', '2 2 2')
-  startTimerEl.addEventListener('click', function () {
-    console.log('start timer clicked')
-    controller.startWork()
-  })
-  sceneEl.appendChild(startTimerEl)
+  constructor () {
+    this.createScene()
+    this.createEnvironment()
+    this.createButtons()
+    this.createTimer()
 
-  const timerEl = document.createElement('a-box')
-  timerEl.setAttribute('color', 'red')
-  timerEl.setAttribute('position', '0 1 -20')
-  timerEl.setAttribute('scale', '2 0 2')
-  sceneEl.appendChild(timerEl)
-  const maxTimerWidth = 20
+    console.log('A-Frame Application initialized')
+  }
 
-  const timeLeftEl = document.createElement('a-text')
-  timeLeftEl.setAttribute('value', model.get('timeLeft').formattedSeconds)
-  timeLeftEl.setAttribute('position', '-1.53731 4.9242 -24.16562')
-  timeLeftEl.setAttribute('scale', '11.61044 13.14882 11.61044')
-  sceneEl.appendChild(timeLeftEl)
+  createScene () {
+    this.sceneEl = document.createElement('a-scene')
+    this.sceneEl.setAttribute('cursor', 'rayOrigin: mouse')
+    document.body.appendChild(this.sceneEl)
+  }
 
-  model.subscribe('timeLeft:changed', (timeLeft: TimeLeft) => {
-    console.log('time left changed', timeLeft)
-    timeLeftEl.setAttribute('value', timeLeft.formattedSeconds)
+  createButtons () {
+    const startTimerEl = document.createElement('a-box')
+    startTimerEl.setAttribute('color', 'green')
+    startTimerEl.setAttribute('position', '-4 1 -10')
+    startTimerEl.setAttribute('scale', '2 2 2')
+    startTimerEl.setAttribute(
+      'animation',
+      'property: position; from: -4 -1 -10; to: -4 1 -10; dur: 2000; easing: easeInCirc'
+    )
 
-    const timerWidth = maxTimerWidth * timeLeft.percentCompleted
+    startTimerEl.addEventListener('click', function () {
+      console.log('start timer clicked')
+      controller.startWork()
+    })
+    this.sceneEl.appendChild(startTimerEl)
+  }
 
-    console.log(timerWidth)
-    //@ts-ignore
-    timerEl.object3D.scale.y = timerWidth
-  })
+  createTimer () {
+    const timerEl = document.createElement('a-box')
+    timerEl.setAttribute('color', 'red')
+    timerEl.setAttribute('position', '0 1 -20')
+    timerEl.setAttribute('scale', '2 0 2')
+    timerEl.setAttribute(
+      'animation',
+      'property: position; from: 0 -1 -20; to: 0 1 -20; dur: 1000; dealy: 500; easing: easeInCirc'
+    )
+    this.sceneEl.appendChild(timerEl)
+    const maxTimerWidth = 20
 
-  const environment = document.createElement('a-entity')
-  environment.setAttribute('environment', 'preset: goldmine')
-  sceneEl.appendChild(environment)
+    const timeLeftEl = document.createElement('a-text')
+    timeLeftEl.setAttribute('value', model.get('timeLeft').formattedSeconds)
+    timeLeftEl.setAttribute('scale', '11.61044 13.14882 11.61044')
+    timeLeftEl.setAttribute('position', '-1.53731 4.9242 -24.16562')
+    timeLeftEl.setAttribute(
+      'animation',
+      'property: position; from: -1.53731 -4.9242 -24.16562; to: -1.53731 4.9242 -24.16562; dur: 2400; dealy: 200; easing: easeInCirc'
+    )
+    this.sceneEl.appendChild(timeLeftEl)
 
-  body.appendChild(sceneEl)
+    model.subscribe('timeLeft:changed', (timeLeft: TimeLeft) => {
+      console.log('time left changed', timeLeft)
+      timeLeftEl.setAttribute('value', timeLeft.formattedSeconds)
 
-  console.log('Hello, A-Frame!')
+      const timerWidth = maxTimerWidth * timeLeft.percentCompleted
+
+      console.log(timerWidth)
+      //@ts-ignore
+      timerEl.object3D.scale.y = timerWidth
+    })
+  }
+
+  createEnvironment () {
+    const environment = document.createElement('a-entity')
+    environment.setAttribute('environment', 'preset: goldmine')
+    this.sceneEl.appendChild(environment)
+  }
 }
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener('DOMContentLoaded', () => new AframeApp())
