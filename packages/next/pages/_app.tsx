4dac3cd40539159d6bec436f06b8b02b51113b79
Dawid Janiga <dawid.janiga@netguru.com>
2022-06-02 00:16:19 +0200

    refactor(next): remove react-query

--- packages/next/pages/_app.tsx
+++ packages/next/pages/_app.tsx
@@ -1,13 +1,6 @@
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 export default function MyApp ({ Component, pageProps }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  )
+  return <Component {...pageProps} />
 }
